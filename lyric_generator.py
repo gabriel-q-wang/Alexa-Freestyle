05675ade3800fd4a3efdc04a64c5160ff81be947
gabriel-q-wang <gwgwang2009@gmail.com>
2019-10-26 11:47:30 -0400

    Added NN architect

--- lyric_generator.py
+++ lyric_generator.py
@@ -8,3 +8,44 @@ from keras.models import Sequential
 from keras.layers import LSTM, Activation, Flatten, Dropout, Dense, Embedding, TimeDistributed, CuDNNLSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
+
+
+dataset = pd.read_csv('taylor_swift_lyrics.csv', encoding = "latin1")
+
+def processFirstLine(lyrics, songID, songName, row):
+    lyrics.append(row['lyric'] + '\n')
+    songID.append( row['year']*100+ row['track_n'])
+    songName.append(row['track_title'])
+    return lyrics,songID,songName
+
+# define empty lists for the lyrics , songID , songName
+lyrics = []
+songID = []
+songName = []
+
+# songNumber indicates the song number in the dataset
+songNumber = 1
+
+# i indicates the song number
+i = 0
+isFirstLine = True
+
+# Iterate through every lyrics line and join them together for each song independently
+for index,row in dataset.iterrows():
+    if(songNumber == row['track_n']):
+        if (isFirstLine):
+            lyrics,songID,songName = processFirstLine(lyrics,songID,songName,row)
+            isFirstLine = False
+        else :
+            #if we still in the same song , keep joining the lyrics lines
+            lyrics[i] +=  row['lyric'] + '\n'
+    #When it's done joining a song's lyrics lines , go to the next song :
+    else :
+        lyrics,songID,songName = processFirstLine(lyrics,songID,songName,row)
+        songNumber = row['track_n']
+        i+=1
+
+
+
+# Define a new pandas DataFrame to save songID , songName , Lyrics in it to use them later
+lyrics_data = pd.DataFrame({'songID':songID, 'songName':songName, 'lyrics':lyrics })
