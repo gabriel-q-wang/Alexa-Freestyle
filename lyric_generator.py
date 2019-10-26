daefcc9544e9fefa425f557c26128cd1c9f45806
gabriel-q-wang <gwgwang2009@gmail.com>
2019-10-26 11:14:59 -0400

    Initialize branch

---
+++ lyric_generator.py
@@ -0,0 +1,10 @@
+import sys
+import json
+import os
+import requests
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM, Activation, Flatten, Dropout, Dense, Embedding, TimeDistributed, CuDNNLSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
