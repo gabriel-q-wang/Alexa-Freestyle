917b29048b58af911bc606df9d3d6514d196a058
gabriel-q-wang <gwgwang2009@gmail.com>
2019-10-26 20:21:27 -0400

    Songs stuff

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import json
 import os
 import requests
 import lyric_generator
+from textgenrnn import textgenrnn
 
 def main():
     print('Please enter an artist\'s name: ')
     artist_name = input()
     lyric_list = lyric_generator.get_lyrics_api(artist_name)
-    lyric_generator.train_model(lyric_list)
+    artist_name = artist_name.replace(' ', '-')
+    print(artist_name)
+    if os.path.exists(artist_name + '.hdf5'):
+        textgen = textgenrnn('textgenrnn_weights.hdf5')
+        textgen.generate_samples()
+    else:
+        lyric_generator.train_model(lyric_list, artist_name)
 
 
 main()
