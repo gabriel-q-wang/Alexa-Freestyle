84108d1248c3cbef67f4cf530af5d431cf44c6f4
gabriel-q-wang <gwgwang2009@gmail.com>
2019-10-27 02:08:16 -0400

    Added backend changes and models

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import os
 import requests
 import lyric_generator
 
-def main():
-    print('Please enter an artist\'s name: ')
-    artist_name = input()
+def main(artist_name):
     lyric_list = lyric_generator.get_lyrics_api(artist_name)
-    lyric_generator.train_model(lyric_list)
-
-
-main()
+    artist_name = artist_name.replace(' ', '-')
+    path_name = artist_name + '.hdf5'
+    if os.path.exists(path_name):
+        textgen = textgenrnn(path_name)
+        response = textgen.generate(1)
+        return response
+    else:
+        lyric_generator.train_model(lyric_list, artist_name)
+        textgen = textgenrnn(path_name)
+        response = textgen.generate(1)
+        return response
